d033c10a4e54ecc81f8d1301d8706b78cac1eae4
RAFAEL ORIOL ORTI CARRIO <raorcar3@gmail.com>
2022-04-14 18:19:35 +0200

    CRUD login view created

--- views/UserRouter.js
+++ views/UserRouter.js
@@ -17,26 +17,44 @@ router.post('/register', UsersController.register);
 router.post('/login', UsersController.login);
 //http://localhost:5000/users/login
 
+//optional to make email confirmation
+// //email confirmation
+// router.post('/confirm', UsersController.confirm);
+// //http://localhost:5000/users/confirm
+
 
 
 //USERS PROFILE VIEW
 
+//update profile
+router.put('/update', auth, UsersController.update);
+//http://localhost:5000/users/update
 
+//read profile
+router.get('/profile', auth, UsersController.profile);
+//http://localhost:5000/users/profile
 
 //delete user by email
-router.delete('/:email', auth, isAdmin, UsersController.delete);
-//http://localhost:5000/users/:email
-
-//delete user by name
-router.delete('/name/:name', auth, isAdmin, UsersController.deleteByName);
-//http://localhost:5000/users/name/:name
+router.delete('/delete', auth, UsersController.delete);
+//http://localhost:5000/users/delete
 
 
 //ADMIN PROFILE VIEW
 
-//read users
-router.get('/', auth, isAdmin, UsersController.read);
+//read users admin
+router.get('/', auth, isAdmin, UsersController.usersRead);
 //http://localhost:5000/users
 
+//create new user admin
+router.post('/', auth, isAdmin, UsersController.adminUserCreate);
+//http://localhost:5000/users
+
+//update profile by email admin
+router.put('/email/:email', auth, isAdmin, UsersController.adminUserUpdateByEmail);
+//http://localhost:5000/users/email/:email
+
+//delete user by email admin
+router.delete('/:email', auth, isAdmin, UsersController.adminUserDelete);
+//http://localhost:5000/users/:email
 
 
