837764f40ddab98e0e9bcc38bdd011169d2ce0c6
RAFAEL ORIOL ORTI CARRIO <raorcar3@gmail.com>
2022-04-14 15:23:57 +0200

    feat: index.js created

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const db = require('./db.js');
+
+const PORT = 5000;
+
+const router = require('./router');
+//Middleware
+let corsOptions = {//CONFIGURO OPCIONES DE CORS PARA PERMITIR QUE ACCIONES DE API NO SEAN DETECTADAS COMO MALWARE
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+};
+
+app.use(express.json()); //PUEDO OBTENER JSON DEL BODY
+app.use(cors(corsOptions));  //USO CORS
+//Middleware
+
+app.use(router);
+
+db.then(()=>{
+    app.listen(PORT, ()=> console.log(`Server on port ${PORT}`)); //Conectado a la base de datos
+})
+.catch((err)=> console.log(err.message));  
