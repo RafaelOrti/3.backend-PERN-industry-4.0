9923a2cace08f7ffbc9f87068a2c4847957f8f33
RAFAEL ORIOL ORTI CARRIO <raorcar3@gmail.com>
2022-05-23 13:23:07 +0200

    installation modbus tcp structure defined

--- controllers/InstallationModbusTCPController.js
+++ controllers/InstallationModbusTCPController.js
@@ -3,31 +3,126 @@ const {
   RealTimeInstallation
 } = require("../models/index");
 const axios = require('axios');
-
-//oauth2.0
-const oauth = require('axios-oauth-client');
-
 const InstallationModbusTCPController = {};
 
+
+console.log("hola")
+
+const net = require('net');
+const modbus = require('jsmodbus')
+const socket = new net.Socket();
+const options = {
+  'host':'192.168.1.11',
+  'port': '502'
+}
+const client = new modbus.client.TCP(socket)
+var myData = 0;
+
+
+InstallationModbusTCPController.communication = (req, res) => {
+
+  try {
+
+
+
+
+  } catch (error) {
+      res.send(error)
+  };
+}
+
+
+
+
 //modbus tcp
-var modbus = require("modbus-tcp");
-var client = new modbus.Client();
-var server = new modbus.Server();
- 
-// link client and server streams together
-client.writer().pipe(server.reader());
-server.writer().pipe(client.reader());
-// if you have a socket (stream) you can just
-// call client.pipe(socket) or server.pipe(socket)
- 
-server.on("read-coils", function (from, to, reply) {
-    return reply(null, [ 1, 0, 1, 1 ]);
-});
- 
-// read coils from unit id = 0, from address 10 to 13
-client.readCoils(0, 10, 13, function (err, coils) {
-    // coils = [ 1, 0, 1, 1 ]
-});
+
+
+
+socket.on('connect',()=>{
+  console.log("Conectado")
+  function getdate(){
+    client.readHoldingRegisters(40,8).then(function(resp){
+      myData=resp.response._body._values[0]
+      console.log("data1",myData)
+      myData=resp.response._body._values[1]
+      console.log("data2",myData)
+      myData=resp.response._body._values[2]
+      console.log("data3",myData)
+      myData=resp.response._body._values[3]
+      console.log("data4",myData)
+    }).catch(function(){
+      console.error(arguments)
+      socket.end()
+    })
+    client.readHoldingRegisters(40,1).then(function(resp){
+      myData=resp.response._body._values[0]
+      console.log("data1",myData)
+      const bit1=Boolean(myData&(1<<0))
+      const bit2=Boolean(myData&(1<<1))
+      const bit3=Boolean(myData&(1<<2))
+      const bit4=Boolean(myData&(1<<3))
+      const bit5=Boolean(myData&(1<<4))
+      const bit6=Boolean(myData&(1<<5))
+      const bit7=Boolean(myData&(1<<6))
+      const bit8=Boolean(myData&(1<<7))
+      const bit9=Boolean(myData&(1<<8))
+      const bit10=Boolean(myData&(1<<9))
+      const bit11=Boolean(myData&(1<<10))
+      const bit12=Boolean(myData&(1<<11))
+      const bit13=Boolean(myData&(1<<12))
+      const bit14=Boolean(myData&(1<<13))
+      const bit15=Boolean(myData&(1<<14))
+      const bit16=Boolean(myData&(1<<15))
+
+      console.log("data1",bit1)
+      console.log("data2",bit2)
+      console.log("data3",bit3)
+      console.log("data4",bit4)
+      console.log("data5",bit5)
+      console.log("data6",bit6)
+      console.log("data7",bit7)
+      console.log("data8",bit8)
+      console.log("data9",bit9)
+      console.log("data10",bit10)
+      console.log("data11",bit11)
+      console.log("data12",bit12)
+      console.log("data13",bit13)
+      console.log("data14",bit14)
+      console.log("data15",bit15)
+      console.log("data16",bit16)
+
+      // const bit16=Boolean(myData&(valor<<16)) para poner primera palabra encima del todo
+      //luego haces suma
+
+
+
+
+
+    }).catch(function(){
+      console.error(arguments)
+      socket.end()
+    })
+  }
+  setInterval(getdate,1000)
+
+})
+
+socket.on('error',()=>{
+  console.log("sin servidor en puerto ",port," de ",host)
+})
+
+// socket.on('data',(data)=>{
+//   console.log("data",data)
+// })
+
+
+socket.connect(options.port,options.host)
+
+
+
+
+
+
 
 
 module.exports = InstallationModbusTCPController;
--- router.js
+++ router.js
@@ -4,15 +4,13 @@ const AdminUserRouter = require('./views/0.AdminUserRouter');
 const ClientAdminUserRouter = require('./views/0.ClientAdminUserRouter');
 const LoginUserRouter = require('./views/0.LoginUserRouter');
 const ProfileUserRouter = require('./views/0.ProfileUserRouter');
-const InstallationRouter = require('./views/InstallationRouter');
-const InstallationHomeRouter = require('./views/InstallationHomeRouter');
 const InstallationModbusTCPRouter = require('./views/InstallationModbusTCPRouter');
 
 // const PeliculasRouter = require('./views/PeliculasRouter');
 // const OrdersRouter = require('./views/OrdersRouter');
 
 router.use('/users', AdminUserRouter,ClientAdminUserRouter,LoginUserRouter,ProfileUserRouter);
-router.use('/installation', InstallationRouter,InstallationHomeRouter,InstallationModbusTCPRouter );
+router.use('/installation', InstallationModbusTCPRouter );
 // router.use('/users1', ClientAdminUserRouter);
 // router.use('/users2', LoginUserRouter);
 // router.use('/users3', ProfileUserRouter);
